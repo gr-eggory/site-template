f0efeb1f53ee8d0f1381c2fcb46cc3807c188c2e
gr-eggory <greggroks@gmail.com>
2023-06-20 20:47:26 -0400

    Prettier

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,22 +1,24 @@
-export const siteTitle = 'My Cool Site'
-export const siteDescription = 'An Easy to Use Site Template'
-export const siteURL = 'example.com'
-export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
-export const siteAuthor = 'Amadeus'
+export const siteTitle = 'My Cool Site';
+export const siteDescription = 'An Easy to Use Site Template';
+export const siteURL = 'example.com';
+export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter';
+export const siteAuthor = 'Amadeus';
 
 // Controls how many posts are shown per page on the main blog index pages
-export const postsPerPage = 10
+export const postsPerPage = 10;
 
 // Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
 export const navItems = [
 	{
 		title: 'Blog',
-		route: '/blog'
-	}, {
-		title: 'About',
-		route: '/about'
-	}, {
-		title: 'Contact',
-		route: '/contact' 
+		route: '/blog',
 	},
-]
+	{
+		title: 'About',
+		route: '/about',
+	},
+	{
+		title: 'Contact',
+		route: '/contact',
+	},
+];
