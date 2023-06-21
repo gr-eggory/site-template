d6e87226a64ea8a9f865df39654eef3dd75331ba
gr-eggory <greggroks@gmail.com>
2023-06-20 20:44:16 -0400

    Added config file

---
+++ src/lib/config.ts
@@ -0,0 +1,22 @@
+export const siteTitle = 'My Cool Site'
+export const siteDescription = 'An Easy to Use Site Template'
+export const siteURL = 'example.com'
+export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
+export const siteAuthor = 'Amadeus'
+
+// Controls how many posts are shown per page on the main blog index pages
+export const postsPerPage = 10
+
+// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+export const navItems = [
+	{
+		title: 'Blog',
+		route: '/blog'
+	}, {
+		title: 'About',
+		route: '/about'
+	}, {
+		title: 'Contact',
+		route: '/contact' 
+	},
+]
