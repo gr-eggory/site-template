ff879896f69c4b8ceb6822beacfa8ef49a37f5c6
gr-eggory <greggroks@gmail.com>
2023-06-21 09:16:20 -0400

    Making post list look better

--- src/routes/(site)/blog/+page.svelte
+++ src/routes/(site)/blog/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { siteConfig } from '$lib/config';
 	import type Post from '$lib/types/post';
 
 	// import Main from '$lib/components/Main.svelte';
@@ -9,16 +10,11 @@
 	export let data: PageData;
 	let posts = data.posts || [];
 	let totalPosts = data.totalPosts.total;
-
 </script>
 
 <svelte:head>
-	<title>Evergreg | Blog</title>
-	<meta
-		data-key="description"
-		name="description"
-		content="Writings on software development, biology, and psychology"
-	/>
+	<title>{siteConfig.title} | Blog</title>
+	<meta data-key="description" name="description" content={siteConfig.description} />
 	<!-- <meta property="og:image" content="https://joshcollinsworth.com/images/site-image.png" /> -->
 	<!-- <meta name="twitter:image" content="https://joshcollinsworth.com/images/site-image.png" /> -->
 </svelte:head>
@@ -31,15 +27,22 @@
 	</div>
 </Main> -->
 
-<div>
+<ul>
 	{#each posts as post (post.title)}
-		<div>
-			<h3>{post.title}</h3>
-			<p>{post.description}</p>
-			<p>Created on {post.date}</p>
-		</div>
+		<li>
+			<article>
+				<a href="/blog/{post.slug}">
+					<!-- img goes here -->
+					<h2>
+						{post.title}
+					</h2>
+					<p>{post.description}</p>
+					<p>Created on {post.date}</p>
+				</a>
+			</article>
+		</li>
 	{/each}
-</div>
+</ul>
 
 <style lang="scss">
 </style>
