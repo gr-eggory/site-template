ac18fc6fae80c3c4c97fdee3a87569e526f60793
gr-eggory <greggroks@gmail.com>
2023-06-21 19:01:20 -0400

    Providing info on how to use template in index page

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to your site!</h1>
+<p>To begin, start editing the config with your info in <code>/src/lib/config.ts</code></p>
+<p>
+	For more information, please visit <a
+		href="https://github.com/gr-eggory/site-template"
+		target="_blank">here</a
+	>.
+</p>
+
+<style>
+	a {
+		color: var(--clr-primary);
+	}
+</style>
