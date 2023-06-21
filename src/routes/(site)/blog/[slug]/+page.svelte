39d795a522a0ebc72f4ca7d4f6c6837034b52236
gr-eggory <greggroks@gmail.com>
2023-06-21 19:06:56 -0400

    Adding base page for individual blog post

---
+++ src/routes/(site)/blog/[slug]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let data;
+
+	const {
+		title,
+		date,
+		description,
+		categories,
+		slug,
+		coverImage,
+		coverWidth,
+		coverHeight,
+		subtitle,
+		updated,
+	} = data.meta;
+	const { PostContent } = data;
+</script>
+
+<svelte:head>
+	<!-- Be sure to add your image files and un-comment the lines below -->
+	<title>{title}</title>
+	<meta data-key="description" name="description" content={description} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={title} />
+	<meta name="twitter:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta name="twitter:description" content={description} />
+	<!-- <meta property="og:image" content="https://yourdomain.com/image_path" /> -->
+	<!-- <meta property="og:image:width" content={coverWidth} />
+	<meta property="og:image:height" content={coverHeight} /> -->
+	<!-- <meta name="twitter:image" content="https://yourdomain.com/image_path" /> -->
+</svelte:head>
+
+<PostContent />
