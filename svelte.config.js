4a7c51594109d923914af6eb46d8a492077d04c6
gr-eggory <greggroks@gmail.com>
2023-06-21 22:35:00 -0400

    Removed unused config object

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,6 @@ import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extension: ['.md'],
-};
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
