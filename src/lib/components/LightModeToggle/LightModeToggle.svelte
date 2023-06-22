f98f43472379f2b5e834483cfe21fab6a0b894d6
gr-eggory <greggroks@gmail.com>
2023-06-22 17:35:08 -0400

    Made LightModeToggle

---
+++ src/lib/components/LightModeToggle/LightModeToggle.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	import type { FlyParams } from 'svelte/transition';
+
+	import { theme, toggleTheme } from '$lib/store';
+
+	const myFly: FlyParams = { y: 10 };
+</script>
+
+<button on:click={toggleTheme} aria-label="Toggle theme">
+	{#if $theme === 'dark'}
+		<div in:fly={myFly}>
+			<!-- sun icon -->
+			<span>Dark</span>
+		</div>
+	{:else}
+		<div in:fly={myFly}>
+			<!-- moon icon -->
+			<span>Light</span>
+		</div>
+	{/if}
+</button>
+
+<style>
+	button {
+		padding: 0;
+		font-weight: inherit;
+		background: none;
+		border: none;
+		box-shadow: none;
+		overflow: hidden;
+	}
+	button > * {
+		display: flex;
+		gap: var(--size-2);
+	}
+</style>
