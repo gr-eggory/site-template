1354b22a92c0ad56830ed5047cd198254f94f13d
gr-eggory <greggroks@gmail.com>
2023-06-21 21:48:14 -0400

    Added gallary page

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,6 @@
+import type { Gallery } from './types/gallery';
+
+// Main config for entire website
 export const siteConfig = {
 	title: 'My Cool Site',
 	description: 'An Easy to Use Site Template',
@@ -9,11 +12,15 @@ export const siteConfig = {
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10;
 
-// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+// Edit this to alter the main nav menu. (Also used by the footer).
 export const navItems = [
 	{
 		title: 'Blog',
 		route: '/blog',
+	},
+	{
+		title: 'Gallery',
+		route: '/gallery',
 	},
 	{
 		title: 'About',
@@ -22,5 +29,36 @@ export const navItems = [
 	{
 		title: 'Contact',
 		route: '/contact',
+	},
+];
+
+export const galleries: Gallery[] = [
+	{
+		title: 'Woodland Photography',
+		slug: 'woodland-photography',
+		excerpt: 'Shots taken in New England',
+		description:
+			'All photos were taken by a Canon EOS R6 Mark II with post-processing in Photoshop.',
+		photos: [
+			'/gallery/woodlands/woods1.jpg',
+			'/gallery/woodlands/woods2.jpg',
+			'/gallery/woodlands/woods3.jpg',
+		],
+	},
+	{
+		title: 'Creature Design',
+		slug: 'creature-design',
+		excerpt: 'Visual studies of speculative evolution.',
+		description:
+			'My inspirations draw primarily from entymology and herpetology. Most works done in pencil and pen, sometimes with watercolor.',
+		photos: [
+			'/gallery/animals/anole.webp',
+			'/gallery/animals/ant.webp',
+			'/gallery/animals/cat.webp',
+			'/gallery/animals/cat.webp',
+			'/gallery/animals/cat.webp',
+			'/gallery/animals/cat.webp',
+			'/gallery/animals/cat.webp',
+		],
 	},
 ];
---
+++ src/routes/(site)/gallery/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { galleries } from '$lib/config';
+</script>
+
+<main>
+	<ul>
+		{#each galleries as gallery (gallery.title)}
+			<li>
+				<a href="/gallery/{gallery.slug}">
+					<h3>{gallery.title}</h3>
+				</a>
+				<p>{gallery.excerpt}</p>
+			</li>
+		{/each}
+	</ul>
+</main>
