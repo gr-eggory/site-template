5f564ed2191649f09fec09b95fb321e9e0ba01a2
gr-eggory <greggroks@gmail.com>
2023-06-22 21:28:31 -0400

    Added pagination component

---
+++ src/lib/components/Pagination/Pagination.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	export let currentPage: number;
+	export let totalPosts: number;
+
+	let pagesAvailable: number;
+	$: pagesAvailable = Math.ceil(totalPosts / 10);
+
+	const isCurrentPage = (page: number): boolean => page === currentPage;
+</script>
+
+{#key currentPage}
+	{#if pagesAvailable > 1}
+		<nav aria-label="Pagination navigation">
+			<h2>Go to page:</h2>
+			<ul>
+				{#each Array.from({ length: pagesAvailable }, (_, i) => i + 1) as page}
+					<li>
+						<a href="/blog/page/{page}" aria-current={isCurrentPage(page)}>
+							{#if isCurrentPage(page)}
+								<span> Current page </span>
+							{:else}
+								<span>Go to page</span>
+							{/if}
+							{page}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+	{/if}
+{/key}
+
+<style lang="scss">
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: start;
+		gap: var(--spacing-8);
+	}
+
+	a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: var(--font-16);
+		font-weight: bold;
+		transition: background 0.1s;
+
+		&:hover {
+			background: var(--clr-secondary);
+		}
+
+		&[aria-current='true'] {
+			background: var(--clr-tertiary);
+			color: var(--clr-tertiary-contrast-txt);
+		}
+	}
+</style>
