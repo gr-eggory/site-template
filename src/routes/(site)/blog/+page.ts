d562ecc4cdcb534a78925536a7b89c887c7a1057
gr-eggory <greggroks@gmail.com>
2023-06-22 22:20:32 -0400

    Adjusting API return value of /blog

--- src/routes/(site)/blog/+page.ts
+++ src/routes/(site)/blog/+page.ts
@@ -1,14 +1,15 @@
 import type Post from '$lib/types/post.js';
+import type { CountRes } from '../../api/posts/count/types.js';
 
 export const load = async ({ fetch }) => {
 	const res = await fetch(`/api/posts/offset/0`);
 	const posts = (await res.json()) as Post[];
 
 	const count = await fetch(`/api/posts/count`);
-	const total = (await count.json()) as { total: number };
+	const { total } = (await count.json()) as CountRes;
 
 	return {
 		posts,
-		totalPosts: total,
+		total,
 	};
 };
