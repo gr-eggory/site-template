494ef5626e972af33ba246d10c47901ab7b85446
gr-eggory <greggroks@gmail.com>
2023-06-22 22:19:09 -0400

    PostsList component

---
+++ src/lib/components/PostsList/PostsList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type Post from '$lib/types/post';
+
+	export let posts: Post[];
+</script>
+
+<ul>
+	{#each posts as post (post.title)}
+		<li>
+			<article>
+				<a href="/blog/{post.slug}">
+					<!-- img goes here -->
+					<h2>
+						{post.title}
+					</h2>
+					<p>{post.description}</p>
+					<p>Created on {post.date}</p>
+				</a>
+			</article>
+		</li>
+	{/each}
+</ul>
