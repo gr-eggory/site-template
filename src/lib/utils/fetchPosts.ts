4197b934f848e510b9f4a4c17901bf9a8cc38657
gr-eggory <greggroks@gmail.com>
2023-07-23 18:44:01 -0400

    Fixing TS errors

--- src/lib/utils/fetchPosts.ts
+++ src/lib/utils/fetchPosts.ts
@@ -13,8 +13,8 @@ const fetchPosts = async ({
 
 	posts = await Promise.all(
 		Object.entries(import.meta.glob(`../content/posts/*.md`)).map(async ([path, page]) => {
-			const { metadata } = await page();
-			const slug = path.split('/').pop().split('.').shift();
+			const { metadata } = (await page()) as { metadata: any };
+			const slug = path.split('/').pop()?.split('.').shift();
 			return { ...metadata, slug };
 		})
 	);
