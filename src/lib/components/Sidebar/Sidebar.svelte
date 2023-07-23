63206d737b2809c32341ccac6f7b00a2efdf5235
gr-eggory <greggroks@gmail.com>
2023-07-23 19:06:24 -0400

    Sidebar component

---
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Tag } from '$lib/types/post';
+
+	// import TagList from './tags/TagList.svelte'
+	// import Tag from './tags/Tag.svelte'
+	// import PostSearch from './PostSearch.svelte'
+
+	export let popularPosts: Tag[];
+	export let allCategories: string[];
+</script>
+
+<aside>
+	<h2>Categories</h2>
+	<ul>
+		{#each allCategories as category}
+			<li>
+				<a href="/blog/category/{category}">{category}</a>
+			</li>
+		{/each}
+	</ul>
+
+	<h2>Popular posts</h2>
+	<ul>
+		{#each popularPosts as post}
+			<li>
+				<a href="/blog/{post.slug}">{post.title}</a>
+			</li>
+		{/each}
+	</ul>
+</aside>
