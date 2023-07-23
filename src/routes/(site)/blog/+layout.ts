9471c335ed38eae7508ac696c6ec22ccdeef15ef
gr-eggory <greggroks@gmail.com>
2023-07-23 19:06:11 -0400

    Typing popularPosts to be Tag[]

--- src/routes/(site)/blog/+layout.ts
+++ src/routes/(site)/blog/+layout.ts
@@ -1,10 +1,11 @@
+import type { Tag } from '$lib/types/post.js';
 import type Post from '$lib/types/post.js';
 
 export const load = async ({ fetch }) => {
 	const res = await fetch(`/api/posts/all`);
 	const resJSON = (await res.json()) as Post[];
 
-	const popularPosts = resJSON
+	const popularPosts: Tag[] = resJSON
 		.filter((post) => post.categories.includes('popular'))
 		.map((post) => ({ slug: post.slug, title: post.title }));
 
