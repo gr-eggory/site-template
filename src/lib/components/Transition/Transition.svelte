46c067846aaf5e8a585b6e3c5f28cec5ffc28252
gr-eggory <greggroks@gmail.com>
2023-07-24 12:37:23 -0400

    Made it so Transition component can now take an array of exception paths

--- src/lib/components/Transition/Transition.svelte
+++ src/lib/components/Transition/Transition.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
+
 	export let url: string;
+	export let exceptions: string[] = [];
+
+	let previousPage = '/';
+	let key = 0;
+
+	$: {
+		(() => {
+			const doNotTriggerTransition = exceptions.some(
+				(exception) => previousPage.startsWith(exception) && url.startsWith(exception)
+			);
+			if (doNotTriggerTransition) {
+				previousPage = url;
+				return;
+			}
+			key++;
+			previousPage = url;
+		})();
+	}
 </script>
 
-{#key url}
+{#key key}
 	<div class="transition" in:fade>
 		<slot />
 	</div>
